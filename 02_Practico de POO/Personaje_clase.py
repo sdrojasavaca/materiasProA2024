1381a721ddff5a902c86768f2f3f159df50dfc1e
sdrojasavaca <162600858+sdrojasavaca@users.noreply.github.com>
2024-09-30 12:22:51 -0300

    Add files via upload

--- 02_Practico de POO/Personaje_clase.py
+++ 02_Practico de POO/Personaje_clase.py
@@ -1,3 +1,4 @@
+import random
 class Personaje:
     #atributo de clase
     estado = True #define el estado del personaje como vivo mientras sea True
@@ -13,21 +14,38 @@ class Personaje:
 
     def atacar(self, otro_jugador):
         if self.estado :
-            danno = self.ataque - otro_jugador.defensa
-            if danno > 0:
-                otro_jugador.vida -= danno
-                print(f"{Personaje.nombre} ataca a {otro_jugador.nombre} causando {danno} de daño")
+            danno = random.randint(0, self.ataque)
+            if danno >= 1:
+                if otro_jugador.defensa >=1:
+                    otro_jugador.defensa -= danno 
+                    print(f"{otro_jugador.nombre} se quedo con {otro_jugador.defesa} de defensa despues del ataque que recibio")
+                else:
+                    otro_jugador.vida -= danno
+                    print(f"{otro_jugador.nombre} se quedo con {otro_jugador.vida} de vida despues del ataque que recibio")
             else:
-                print(f"{Personaje.nombre} ataca a {otro_jugador.nombre} sin causar daño")
+                print(f"{self.nombre} no provoca nada de daño en su ataque")
         else:
-            print (f"{Personaje.nombre} no puede atacar, esta muerto")
+            print (f"{self.nombre} no puede atacar, esta muerto")
+            
     def recibirDano(self, otro_jugador):
-        if self.estado :
-            danno = otro_jugador.ataque - self.defensa
-            if danno > 0:
-                self.vida -= danno
-                print(f"{Personaje.nombre} ataca a {otro_jugador.nombre} causando {danno} de daño")
+        
+        if otro_jugador.estado :
+            danno = random.randint(0, otro_jugador.ataque)
+            if danno >= 1:
+                if self.defensa >=1:
+                    self.defensa -= danno 
+                    print(f"{self.nombre} se quedo con {self.defesa} de defensa despues del ataque que recibio")
+                else:
+                    self.vida -= danno
+                    print(f"{self.nombre} se quedo con {self.vida} de vida despues del ataque que recibio")
             else:
-                print(f"{Personaje.nombre} ataca a {otro_jugador.nombre} sin causar daño")
+                print(f"{otro_jugador.nombre} no provoca nada de daño en su ataque")
         else:
-            print (f"{Personaje.nombre} no puede recibir daño, esta muerto")
+            print (f"{otro_jugador.nombre} no puede atacar, esta muerto")
+        if self.vida <=0:
+            self.estado=False
+            print (f"{self.nombre} ha muerto")
+        else:
+            None
+            #TIENE QUE RESTAR TAMBIEN Defensa
+            #RANDOM para el ataque que sea que dentro de un paramentro segun la defensa del personaje sea mayor o menor la defensa. pero dentro de una aleatoriedad
---
+++ 02_Practico de POO/carrera.py
@@ -0,0 +1,90 @@
+from Personaje_clase import Personaje
+personajes = []
+def crearPersonaje():
+    nombre = input("Ingresa el nombre del personaje: ")
+    while True:
+        try:
+            ataque = int(input(f"Ingresa el ataque de {nombre} 1/100: "))
+            if 1 <= ataque <= 100:
+                break
+            else:
+                print("el valor del ataque debe ser entre 1 y 100")
+        except ValueError:
+            print("Entrada inválida. Ingresa un número entero.")
+    while True:
+        try:
+            defensa = int(input(f"Ingresa la defensa de {nombre} 1/100: "))
+            if 1 <= defensa <= 100:
+                break
+            else:
+                print("el valor de la defensa debe ser entre 1 y 100")
+        except ValueError:
+            print("Entrada inválida. Ingresa un número entero.")
+    return Personaje(nombre, ataque, defensa)
+
+def mostrarInformacion(personajes):
+    for i in range(len(personajes)):
+        if personajes.estado:
+            print(f"Personaje {i+1}: {personajes.nombre}, Ataque: {personajes.ataque}, Defensa: {personajes.defensa},Estado: Vivo, Vida: {personajes.vida}")
+        else:
+            print(f"Personaje {i+1}: {personajes.nombre}, Ataque: {personajes.ataque}, Defensa: {personajes.defensa},Estado: Muerto, Vida: {personajes.vida}")
+
+def simular_ataque(personajes):
+    if len(personajes) < 2:
+        print("No hay suficientes personajes para simular un ataque.")
+        return
+    else:
+        mostrarInformacion(personajes)
+        
+        atacanteIndice = int(input("Elige el número del personaje que atacará: ")) - 1
+        defensorIndice= int(input("Elige el número del personaje que recibirá el ataque: ")) - 1
+
+        if atacanteIndice == defensorIndice:
+            print("No se puede atacar a si mismo el pesronaje")
+            return
+        else:
+            atacante = personajes[atacanteIndice]
+            defensor = personajes[defensorIndice]
+
+            atacante.atacar(defensor)
+            if defensor.estado:
+                defensor.atacar(atacante)
+
+            mostrarInformacion(personajes)
+
+def juego():
+        while True:
+            print('''
+                ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+                ▐⑴ Jugar                ▌
+                ▐⑵ Añadir jugador       ▌
+                ▐⑶ Mostrar informacion  ▌
+                ▐⑷ Salir                ▌  
+                ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀''')
+            opcion = input("Elige una opción: ")
+
+            if opcion == "1":
+                if len(personajes) >= 2:
+                    simular_ataque(personajes)
+                else:
+                    print("Tiene que haber minimo 2 personajes creados")
+
+            elif opcion == "2":
+                cantidad = int(input("¿Cuantos personajes queres crear? "))
+                for i in range(cantidad):
+                    personajes.append(crearPersonaje())
+                    if i < cantidad - 1 : 
+                        print("\n Crea el siguiente personaje \n")
+
+            elif opcion == "3":
+                if personajes:
+                    mostrarInformacion(personajes)
+                else:
+                    print("Antes tenes que crear personajes")
+            
+            elif opcion == "4":
+                print("Saliendo del juego.")
+                break
+            else:
+                print("Opción no erronea, intenta de nuevo")
+juego()
